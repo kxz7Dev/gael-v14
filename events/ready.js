dd617f0c06c4f34d2dff7e58fd9f3ab32ab42c78
kxz7 <109927162+kxz7Dev@users.noreply.github.com>
2022-08-10 20:44:27 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const client = require("../index");
 const { ActivityType } = require("discord.js")
 
 client.on("ready", async () => {
-  console.log(`${client.user.username} Iniciado`);
+  console.log(`${client.user.username} Online`);
   client.user.setPresence({
     activities: [{ name: `Update Gael | V13 > V14`, type: ActivityType.Listening }],
     status: 'dnd',
