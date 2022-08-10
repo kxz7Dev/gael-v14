2b7297747d52ce0c53a6deddd79bfaed0470178f
kxz7 <109927162+kxz7Dev@users.noreply.github.com>
2022-08-10 20:10:36 -0300

    Create config.js

---
+++ settings/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+  token: "OTk4NjQ0MTYxMTAyMjE3MjM2.GOg2UC.9snvi8WvguUzzv40SOmZPLAhNNgr__dtabTT0M",
+
+  prefix: ".",
+
+  embed: {
+
+    color: "Random",
+
+    wrongcolor: "#303136"
+
+  },
+
+  emoji: {
+
+    ERROR: "❌",
+
+    SUCCESS: "✅",
+
+    disabled: "🔴",
+
+    enabled: "🟢",
+
+    cleared: "🧹",
+
+    time: "⏲️",
+
+    search: "🔎",
+
+    ping: "🏓",
+
+    bot: "🤖",
+
+  },
+
+  // others
+
+  guildID: "934801432023011368", 
+
+}
